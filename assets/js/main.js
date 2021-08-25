5f782bc87149270cb6552b689b65141445ac5aa8
takataka0206 <89508585+takataka0206@users.noreply.github.com>
2021-08-25 17:34:48 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,7 @@ const app = Vue.createApp({
         },
         stage2: {
           q1: 'いいい',
-          // q2: 'えええ',
+          q2: 'えええ',
           // q3: 'おおお'
         },
         stage3: {
@@ -41,7 +41,7 @@ const app = Vue.createApp({
         ],
         stage2: [
           false, // 2-1
-          // false, // 2-2
+          false, // 2-2
           // false, // 2-3
         ],
         stage3: [
